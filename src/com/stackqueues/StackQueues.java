3179376186018c467b0324d65070a42b00d958ae
jayashri23 <jsjankar23@gmail.com>
2022-12-17 05:12:50 +0530

    Peack and Pop elements

--- src/com/stackqueues/StackQueues.java
+++ src/com/stackqueues/StackQueues.java
@@ -7,10 +7,13 @@ public class StackQueues {
         int data; // integer data
         Node next; // reference variable Node type
     }
+
     Node top;     // create global top reference variable global
+
     StackQueues() {
         this.top = null;
     }
+
     public void push(int a) // insert at the beginning
     {
         Node temp = new Node();     // create new node temp and allocate memory
@@ -22,10 +25,38 @@ public class StackQueues {
         temp.next = top;
         top = temp;
     }
+
+    boolean isEmpty() {  //check if stack is empty or not
+        return top == null;
+    }
+
+
+    public int peek()  //return top element in a stack
+
+    {
+        if (!isEmpty()) {  // If stack is not empty , return the top element
+            return top.data;
+
+        } else
+            System.out.println("Stack is empty");
+
+        return -1;
+    }
+
+    void pop()// Remove a key from given queue
+    {
+        if (top == null) {
+            System.out.println("\n Stack is empty");
+            return;
+        }
+        top = top.next;
+
+    }
+
     public void printData()
     {
         if (top == null) {
-            System.out.printf("\nStack Underflow");
+            System.out.printf("Stack is empty");
             exit(1);
         }
         else {
@@ -39,12 +70,23 @@ public class StackQueues {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         StackQueues obj = new StackQueues();
         System.out.println("\nStack and Queue Operation");
         obj.push(70);
         obj.push(30);
         obj.push(56);
+        System.out.println("\nAdding Element in Stack: ");
         obj.printData();
-          }
-        }
+        System.out.printf("\n\nTop of Element is: %d\n",obj.peek());
+        obj.printData();
+        System.out.println("\n\nRemoving Element in Stack: ");
+        obj.pop();
+        obj.pop();
+        obj.pop();
+        obj.printData();
+
+
+    }
+}
